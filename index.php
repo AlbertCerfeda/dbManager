9913f0180bbc6919d94f35281c7bf1a3f29d6a32
AlbertCerfeda <cerfeda.albert@gmail.com>
2020-03-12 17:29:05 +0100

    Added bootstrap's collapse

--- index.php
+++ index.php
@@ -1,31 +1,39 @@
 <?php 
-    	session_start();
-    	 function drawStatusbar(/*$conn,$phase*/){
-        	
-    	};
-    	echo '<script> function setCookie(cname, cvalue, exdays) {
-			var d = new Date();
-			d.setTime(d.getTime() + (exdays*24*60*60*1000));
-			var expires = "expires="+ d.toUTCString();
-			document.cookie = cname + "=" + cvalue + ";" + expires;
-		  } 
-		  function logout(){
-			setCookie("phase","login",0);
-			window.location.href= "./index.php";
-			} </script>';
+    session_start();
+	 function drawStatusbar(/*$conn,$phase*/){
 
-		//TODO: Fix this fucking issue
-    	if (!isset($_COOKIE['phase']) || !isset($_SESSION['conn'])) {
-    		echo "<script> setCookie('phase','login',2); </script>";
-    		
-    		echo "COOKIE['phase'] is not set. Setting...<br>";
-        }
+	 };
+    echo '<script> function setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + (exdays*24*60*60*1000));
+		var expires = "expires="+ d.toUTCString();
+		document.cookie = cname + "=" + cvalue + ";" + expires;
+	  } 
+	  function logout(){
+		setCookie("phase","login",0);
+		window.location.href= "./index.php";
+	} </script>';
+
+	//TODO: Fix this fucking issue
+   	if (!isset($_COOKIE['phase']) || !isset($_SESSION['conn'])) {
+   		echo "<script> setCookie('phase','login',2); </script>";
+   		
+   		echo "COOKIE['phase'] is not set. Setting...<br>";
+    }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>dbManager</title>
     <link rel="stylesheet" href="style.css">
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"> </script>
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"> </script>
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"> </script>
 </head>
 <body>
 
@@ -70,12 +78,12 @@
 				else{//Credenziali corrette
 					$result = mysqli_query($conn, "SHOW DATABASES");
 					if(@mysqli_num_rows($result)!=0){
-						echo "<script> function tableView(tableName){
+						echo "<script> function dbView(dbName){
 							setCookie('phase','tableView',2);
-							window.location.href= './index.php?table=' + tableName;
+							window.location.href= './index.php?db=' + dbName;
 						} </script>";
 						while($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
-							echo "<input type='button' onClick='tableView(this.value)' value='".$row[0]."'><br>";
+							echo "<input type='button' onClick='dbView(this.value)' value='".$row[0]."'><br>";
 						}
 					}
 				}
@@ -84,6 +92,36 @@
 			break;
 			
 			case 'tableView':
+				$conn = @mysqli_connect($_SESSION['host'], $_SESSION['user'], $_SESSION['pwd']);
+				if (mysqli_connect_errno()){//Credenziali errate
+					echo"<script> logout(); </script>";
+				}
+				else{//Credenziali corrette
+					if(!isset($_GET['db'])){
+						echo "<script> 
+							setCookie('phase','dbView',2);
+							window.location.href= './index.php?';
+						</script>";
+					}else{
+						echo "<h1> Tables </h1>";
+						mysqli_query($conn, "USE ".$_GET['db']);
+						$result = mysqli_query($conn, "SHOW TABLES");
+						
+						if(@mysqli_num_rows($result)!=0){
+							echo "<script> function tableView(tableName){
+							setCookie('phase','tableView',2);
+							window.location.href= './index.php?db=' + tableName;
+							} </script>";
+							while($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
+								echo '<button type="button" data-toggle="collapse" data-target="#'.$row[0].'TABLE">'.$row[0].'</button> <br>';
+								echo '<div id="'.$row[0].'TABLE" class="collapse">
+								<p>Rinchiudimi</p>
+								</div>';
+							}
+						}
+					}
+					
+				}
 				echo "<br><input type='button' onClick='logout()' value='Logout'>";
 			break;
         }
